293af27970ce6cc84eac9fde1956e88908857271
swordzeng <sword_zeng@hotmail.com>
2019-09-16 18:07:03 +0800

    main form

--- AMS_main.py
+++ AMS_main.py
@@ -6,30 +6,40 @@ from first import Ui_First
 from second import Ui_Second
  
 class Ui_MainWindow(QWidget):
-    def setupUi(self, MainWindow):
+    def __init__(self, parent=None):
+        QtWidgets.QWidget.__init__(self, parent=None)
         
+        self.setWindowTitle('AMS')
+        self.resize(500,300)
+
         self.first = First()
         self.second = Second()
 
-        frameMenu = QtWidgets.QFrame()
-        vbox = QtWidgets.QVBoxLayout(frameMenu)
+        vSplit = QtWidgets.QSplitter(QtCore.Qt.Vertical)
         btnReport = QtWidgets.QPushButton('REPORT')
         btnReport.setObjectName('REPORT')
-        vbox.addWidget(btnReport)
+        vSplit.addWidget(btnReport)
         btnFunc = QtWidgets.QPushButton('FUNCTION')
         btnFunc.setObjectName('FUNCTION')
-        vbox.addWidget(btnFunc)
-        vbox.addStretch(1)
+        vSplit.addWidget(btnFunc)
+        #vbox.addStretch(1)
 
-        self.splitter.addWidget(frameMenu)
+        """
+        self.hbox = QtWidgets.QHBoxLayout(self)
+        self.hbox.addLayout(vbox)
+        self.hbox.addWidget(self.first)
+        """
+        
+        self.splitter = QtWidgets.QSplitter(QtCore.Qt.Horizontal)
+        self.splitter.addWidget(vSplit)
         self.splitter.addWidget(self.first)
- 
-        btnReport.clicked.connect(lambda :self.change(self.btnReport.objectName()))
-        btnFunc.clicked.connect(lambda :self.change(self.btnFunc.objectName()))
 
-        hbox = QtWidgets.QHBoxLayout()
+        hbox = QtWidgets.QHBoxLayout(self)
         hbox.addWidget(self.splitter)
         self.setLayout(hbox)
+
+        btnReport.clicked.connect(lambda :self.change(btnReport.objectName()))
+        btnFunc.clicked.connect(lambda :self.change(btnFunc.objectName()))
 
     def change(self,name):
         if name == "REPORT":
@@ -39,7 +49,7 @@ class Ui_MainWindow(QWidget):
         if name == "FUNCTION":
             self.splitter.widget(1).setParent(None)
             self.splitter.insertWidget(1, self.second)
- 
+        
 class First(QWidget, Ui_First):
     def __init__(self):
         super(First,self).__init__()
@@ -53,15 +63,15 @@ class First(QWidget, Ui_First):
 class Second(QWidget, Ui_Second):
     def __init__(self):
         super(Second,self).__init__()
-        self.setupUi(self)
+        self.initUI(self)
         self.setMinimumWidth(30)
         self.setMinimumHeight(30)
  
 if __name__ == '__main__':
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
+    #MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
+    #ui.setupUi(MainWindow)
+    ui.show()
     sys.exit(app.exec_())
