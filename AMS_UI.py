86b907d12a7dd135ab25d93cda03279b12899877
swordzeng <sword_zeng@hotmail.com>
2019-11-25 17:28:55 +0800

    imporve tool bar

--- AMS_UI.py
+++ AMS_UI.py
@@ -32,31 +32,39 @@ class Ui_MainWindow(QMainWindow):
         toolBar.addWidget(spacer)
 
         btnReport = QPushButton("报告生成")
-        btnReport.setFixedWidth(120)
         menuReport = QMenu()
-        menuReport.addAction("aaa")
-        menuReport.addAction("bbb")
+        menuReport.setFixedWidth(120)
+        menuReport.addAction("概览")
+        menuReport.addAction("持仓分析")
         btnReport.setMenu(menuReport)
         toolBar.addWidget(btnReport)
 
         btnFunc = QPushButton("交易管理")
-        btnFunc.setFixedWidth(120)
         menuFunc = QMenu()
         menuFunc.setFixedWidth(120)
         menuFunc.setStyleSheet("text-align:center")
-        menuFunc.addAction("aaa")
-        menuFunc.addAction("bbb")
+        menuFunc.addAction("交易录入")
+        menuFunc.addAction("交易查看")
         btnFunc.setMenu(menuFunc)
         toolBar.addWidget(btnFunc)
 
         btnSpace = QPushButton("")
-        btnSpace.setFixedWidth(60)
+        btnSpace.setFixedWidth(20)
         toolBar.addWidget(btnSpace)
 
         #添加border:none才能将背景色应用到整个tool bar
         toolBar.setStyleSheet("background-color:black; border:none")
-        btnReport.setStyleSheet("color:white")
-        btnFunc.setStyleSheet("color:white")
+        self.setStyleSheet('''
+            QPushButton{color:white;height:40;width:120;font-family:SimHei;font-size:16px;}
+            QPushButton::menu-indicator{image:none}
+            ''')
+
+        btnFunc.setStyleSheet('''
+            QMenu {border-radius:5px;font-family:SimHei;font-size:16px;}
+            QMenu::item {height:40px; width:120px;padding-left:30px;border: 1px solid none;}
+            QMenu::item:selected {background-color:rgb(0,120,215);padding-left:25px;border: 1px solid rgb(65,173,255);}
+            ''')
+
 
 if __name__ == '__main__':
     #字体大小自适应分辨率
