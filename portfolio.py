e6e276e3e02b4b2964ff3d5eb715fd43cb3e9339
swordzeng <sword_zeng@hotmail.com>
2019-12-20 14:23:05 +0800

    get realtime price

--- portfolio.py
+++ portfolio.py
@@ -1,10 +1,11 @@
 import sqlite3
+import requests
 import pandas as pd
 
 def get_hold(dt, account):
     db = sqlite3.connect('AMS.db')
 
-    query = "SELECT SymbolCode, SymbolName, AssetClass,Sector FROM Symbol_Table"
+    query = "SELECT SymbolCode, SymbolName, Market, AssetClass,Sector FROM Symbol_Table"
     df_symbol = pd.read_sql(query, con=db)
 
     query = """
@@ -35,11 +36,14 @@ def get_hold(dt, account):
     df_cur_sum.index.name = 'SymbolCode'
     df_cur_sum['CostAmt'] = df_cur_sum['Qty']
 
+    #合并股票持仓和现金持仓
     df_hold = df_stock_sum.append(df_cur_sum)
     df_hold = df_hold[~df_hold['Qty'].isin([0])]    #去除报告日持仓为0的标的
     df_hold = pd.merge(df_hold, df_symbol, on='SymbolCode', how='left') #补充名称和行业
     
     df_hold['Date'] = dt
+
+    df_hold = get_realtime_price(df_hold)
 
     return df_hold
 
@@ -60,3 +64,25 @@ def get_cost(code, qt, df):
             qt = qt - qt_trans
     
     return amt_cost
+
+def get_realtime_price(df):
+    df['Price'] = 0.0
+
+    for index, row in df.iterrows():
+        if row['AssetClass'] == 'CASH':
+            df.loc[index, 'Price'] = 1.0
+        elif row['AssetClass'] == 'EQUITY':
+            market = df.loc[index,'Market']
+            code = market.lower() + df.loc[index,'SymbolCode'].split('.')[0]
+            url = 'http://hq.sinajs.cn/?format=text&list={}'.format(code)
+            print(url)
+            price_text = requests.get(url).text
+            price_list = price_text.split(',')
+            print(price_text)
+            if market =='HK':
+                price = price_list[5]
+            else:
+                price = price_list[3]
+            df.loc[index, 'Price'] = price
+
+    return df
